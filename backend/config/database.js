1f35f49e6fbe190ae48293fc19f7de3330e8e061
bireswarghosh <116499672+bireswarghosh@users.noreply.github.com>
2023-12-20 11:41:56 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,8 @@
-
-
-
-
-// add db
-
 import mongoose from "mongoose";
 
-export const connectDB = async () => {
- // Set the `strictQuery` option before connecting to suppress the deprecation warning
-mongoose.set('strictQuery', true); // Use true to enforce strict queries, or false to allow non-schema properties in queries
-
- const { connection } = await mongoose.connect(process.env.MONGO_URI);
- console.log(`Database is connected with ${connection.host}`);
+export const connectDatabase = async () => {
+  // Set the `strictQuery` option before connecting to suppress the deprecation warning
+  mongoose.set('strictQuery', true); // Use true to enforce strict queries, or false to allow non-schema properties in queries
+  const { connection } = await mongoose.connect(process.env.MONGO_URI);
+  console.log(`Database is connected with ${connection.host}`);
 };
